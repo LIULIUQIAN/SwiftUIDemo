5fb22a6c2a2ad43a784508582832778bdcca086a
liuliuqian <1414231501@qq.com>
2020-05-21 09:29:54 +0800

    创建项目

---
+++ WeiBoDemo/WeiBoDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiBoDemo
+//
+//  Created by Lct on 2020/5/21.
+//  Copyright © 2020 Lct. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
